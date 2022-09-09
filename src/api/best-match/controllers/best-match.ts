a673d9f2b9e395485bb1e0aec8128cc0bbdf5325
Kris Stern <krisstern@outlook.com>
2022-09-10 04:09:16 +0800

    Add schema for postgres database

---
+++ src/api/best-match/controllers/best-match.ts
@@ -0,0 +1,7 @@
+/**
+ * best-match controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::best-match.best-match');
---
+++ src/api/best-match/routes/best-match.ts
@@ -0,0 +1,7 @@
+/**
+ * best-match router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::best-match.best-match');
