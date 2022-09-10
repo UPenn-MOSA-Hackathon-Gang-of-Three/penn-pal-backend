f085cb00bb33762f3858a177caa00a3c61e85656
Kris Stern <krisstern@outlook.com>
2022-09-10 13:34:41 +0800

    Comment out unused code

--- src/index.ts
+++ src/index.ts
@@ -38,114 +38,101 @@ module.exports = {
    * This gives you an opportunity to extend code.
    */
   register({ strapi }) {
-    strapi.container.get("auth").register("content-api", {
-      name: "custom-jwt-verifier",
-      authenticate: async function (ctx) {
-        try {
-          // get JWT from context and validate.
-          const { authorization } = ctx.request.header;
-
-          if (!authorization) {
-            return { authenticated: false };
-          }
-
-          const token = await getService('jwt').getToken(ctx);
-
-          // const parts = authorization.split(/\s+/);
-          //
-          // if (parts[0].toLowerCase() !== 'bearer' || parts.length !== 2) {
-          //   return { authenticated: false };
-          // }
-          //
-          // const token = parts[1];
-          // const { payload, isValid } = getService('token').decodeJwtToken(token);
-          //
-          // if (!isValid) {
-          //   return { authenticated: false };
-          // }
-
-          if (token) {
-            const {id} = token;
-
-            if (id === undefined) {
-              return { authenticated: false };
-            }
-
-            // fetch authenticated user
-            const user = await getService('user').fetchAuthenticatedUser(id);
-
-            if (!user) {
-              return { error: 'Invalid credentials' };
-            }
-
-            const advancedSettings = await getAdvancedSettings();
-
-            if (advancedSettings.email_confirmation && !user.confirmed) {
-              return { error: 'Invalid credentials' };
-            }
-
-            if (user.blocked) {
-              return { error: 'Invalid credentials' };
-            }
-
-            ctx.state.user = user;
-
-            return {
-              authenticated: true,
-              credentials: user,
-            };
-          }
-
-          const publicPermissions = await strapi.query('plugin::users-permissions.permission').findMany({
-            where: {
-              role: { type: 'public' },
-            },
-          });
-
-          if (publicPermissions.length === 0) {
-            return { authenticated: false };
-          }
-
-          return {
-            authenticated: true,
-            credentials: null,
-          };
-        } catch (err) {
-          return {authenticated: false };
-        }
-      },
-      verify: async function (ctx) {
-        console.log("Need to verify user credentials here.");
-        const { credentials: user } = ctx.state.auth;
-
-        // if (!ctx.config.scope) {
-        //   if (!user) {
-        //     // A non authenticated user cannot access routes that do not have a scope
-        //     throw new UnauthorizedError();
-        //   } else {
-        //     // An authenticated user can access non scoped routes
-        //     return;
-        //   }
-        // }
-        //
-        // let {allowedActions} = ctx.state.auth;
-        //
-        // if (!allowedActions) {
-        //   const permissions = await strapi.query('plugin::users-permissions.permission').findMany({
-        //     where: { role: user ? user.role.id : { type: 'public' } },
-        //   });
-        //
-        //   allowedActions = map('action', permissions);
-        //   ctx.state.auth.allowedActions = allowedActions;
-        // }
-        //
-        // const isAllowed = castArray(ctx.config.scope).every((scope) => allowedActions.includes(scope));
-        //
-        // if (!isAllowed) {
-        //   throw new ForbiddenError();
-        // }
-      }
-    });
+    // strapi.container.get("auth").register("content-api", {
+    //   name: "custom-jwt-verifier",
+    //   authenticate: async function (ctx) {
+    //     try {
+    //       // get JWT from context and validate
+    //       const { authorization } = ctx.request.header;
+    //
+    //       if (!authorization) {
+    //         return { authenticated: false };
+    //       }
+    //
+    //       const token = await getService('jwt').getToken(ctx);
+    //
+    //       if (token) {
+    //         const {id} = token;
+    //
+    //         if (id === undefined) {
+    //           return { authenticated: false };
+    //         }
+    //
+    //         // fetch authenticated user
+    //         const user = await getService('user').fetchAuthenticatedUser(id);
+    //
+    //         if (!user) {
+    //           return { error: 'Invalid credentials' };
+    //         }
+    //
+    //         const advancedSettings = await getAdvancedSettings();
+    //
+    //         if (advancedSettings.email_confirmation && !user.confirmed) {
+    //           return { error: 'Invalid credentials' };
+    //         }
+    //
+    //         if (user.blocked) {
+    //           return { error: 'Invalid credentials' };
+    //         }
+    //
+    //         ctx.state.user = user;
+    //
+    //         return {
+    //           authenticated: true,
+    //           credentials: user,
+    //         };
+    //       }
+    //
+    //       const publicPermissions = await strapi.query('plugin::users-permissions.permission').findMany({
+    //         where: {
+    //           role: { type: 'public' },
+    //         },
+    //       });
+    //
+    //       if (publicPermissions.length === 0) {
+    //         return { authenticated: false };
+    //       }
+    //
+    //       return {
+    //         authenticated: true,
+    //         credentials: null,
+    //       };
+    //     } catch (err) {
+    //       return {authenticated: false};
+    //     }
+    //   },
+    //   verify: async function (ctx) {
+    //     console.log("Need to verify user credentials here.");
+    //     const { credentials: user } = ctx.state.auth;
+    //
+    //     if (!ctx.config.scope) {
+    //       if (!user) {
+    //         // A non authenticated user cannot access routes that do not have a scope
+    //         throw new UnauthorizedError();
+    //       } else {
+    //         // An authenticated user can access non scoped routes
+    //         return;
+    //       }
+    //     }
+    //
+    //     let {allowedActions} = ctx.state.auth;
+    //
+    //     if (!allowedActions) {
+    //       const permissions = await strapi.query('plugin::users-permissions.permission').findMany({
+    //         where: { role: user ? user.role.id : { type: 'public' } },
+    //       });
+    //
+    //       allowedActions = map('action', permissions);
+    //       ctx.state.auth.allowedActions = allowedActions;
+    //     }
+    //
+    //     const isAllowed = castArray(ctx.config.scope).every((scope) => allowedActions.includes(scope));
+    //
+    //     if (!isAllowed) {
+    //       throw new ForbiddenError();
+    //     }
+    //   }
+    // });
   },
 
   /**
@@ -156,19 +143,19 @@ module.exports = {
    * run jobs, or perform some special logic.
    */
   bootstrap({ strapi }) {
-    const admin = require("firebase-admin");
-
-    const firebaseConfig = {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID,
-      measurementId: process.env.FIREBASE_MEASUREMENT_ID
-    };
-
-    admin.initializeApp(firebaseConfig);
-    strapi.firebase = admin;
+    // const admin = require("firebase-admin");
+    //
+    // const firebaseConfig = {
+    //   apiKey: process.env.FIREBASE_API_KEY,
+    //   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    //   projectId: process.env.FIREBASE_PROJECT_ID,
+    //   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    //   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    //   appId: process.env.FIREBASE_APP_ID,
+    //   measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    // };
+    //
+    // admin.initializeApp(firebaseConfig);
+    // strapi.firebase = admin;
   },
 };
