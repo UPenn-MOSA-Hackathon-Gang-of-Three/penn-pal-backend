0588e3ebf5b4f697490e95010d29a30073f0f1af
Kris Stern <krisstern@outlook.com>
2022-09-10 18:00:34 +0800

    Remove unneeded code

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,3 @@
-// export default {
-//   /**
-//    * An asynchronous register function that runs before
-//    * your application is initialized.
-//    *
-//    * This gives you an opportunity to extend code.
-//    */
-//   register(/*{ strapi }*/) {},
-//
-//   /**
-//    * An asynchronous bootstrap function that runs before
-//    * your application gets started.
-//    *
-//    * This gives you an opportunity to set up your data model,
-//    * run jobs, or perform some special logic.
-//    */
-//   bootstrap(/*{ strapi }*/) {},
-// };
-
 'use strict';
 
 require('dotenv').config();
