6188e31eadb4c65ee40be7d3107b5f4f4cb7735b
Kris Stern <krisstern@outlook.com>
2022-09-10 12:36:35 +0800

    Initial setup for firebase authentication

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,109 @@
-export default {
+// export default {
+//   /**
+//    * An asynchronous register function that runs before
+//    * your application is initialized.
+//    *
+//    * This gives you an opportunity to extend code.
+//    */
+//   register(/*{ strapi }*/) {},
+//
+//   /**
+//    * An asynchronous bootstrap function that runs before
+//    * your application gets started.
+//    *
+//    * This gives you an opportunity to set up your data model,
+//    * run jobs, or perform some special logic.
+//    */
+//   bootstrap(/*{ strapi }*/) {},
+// };
+
+'use strict';
+
+require('dotenv').config();
+
+const { castArray, map } = require('lodash/fp');
+const { ForbiddenError, UnauthorizedError } = require('@strapi/utils').errors;
+
+const { getService } = require("@strapi/plugin-users-permissions/server/utils");
+
+const getAdvancedSettings = () => {
+  return strapi.store({ type: 'plugin', name: 'users-permissions' }).get({ key: 'advanced' });
+};
+
+module.exports = {
   /**
    * An asynchronous register function that runs before
    * your application is initialized.
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {
+    strapi.container.get("auth").register("content-api", {
+      name: "custom-jwt-verifier",
+      authenticate: async function (ctx) {
+        // Get JWT from context and validate.
+        const { authorization } = ctx.request.header;
+
+        if (!authorization) {
+          return { authenticated: false };
+        }
+        //This is a hardcoded user id for now, would change this to fetch based off email from the firebase id token validation phase
+        const user = await getService("user").fetchAuthenticatedUser(1);
+
+        if (!user) {
+          return { error: "Invalid credentials" };
+        }
+
+        const advancedSettings = await getAdvancedSettings();
+
+        if (advancedSettings.email_confirmation && !user.confirmed) {
+          return { error: 'Invalid credentials' };
+        }
+
+        if (user.blocked) {
+          return { error: 'Invalid credentials' };
+        }
+
+        ctx.state.user = user;
+
+        return {
+          authenticated: true,
+          credentials: user,
+        };
+      },
+      verify: async function (ctx) {
+        console.log("Need to verify user credentials here.");
+        const { credentials: user } = ctx.state.auth;
+
+        if (!ctx.config.scope) {
+          if (!user) {
+            // A non authenticated user cannot access routes that do not have a scope
+            throw new UnauthorizedError();
+          } else {
+            // An authenticated user can access non scoped routes
+            return;
+          }
+        }
+
+        let {allowedActions} = ctx.state.auth;
+
+        if (!allowedActions) {
+          const permissions = await strapi.query('plugin::users-permissions.permission').findMany({
+            where: { role: user ? user.role.id : { type: 'public' } },
+          });
+
+          allowedActions = map('action', permissions);
+          ctx.state.auth.allowedActions = allowedActions;
+        }
+
+        const isAllowed = castArray(ctx.config.scope).every((scope) => allowedActions.includes(scope));
+
+        if (!isAllowed) {
+          throw new ForbiddenError();
+        }
+      }
+    });
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
@@ -14,5 +112,20 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+    const admin = require("firebase-admin");
+
+    const firebaseConfig = {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    };
+
+    admin.initializeApp(firebaseConfig);
+    strapi.firebase = admin;
+  },
 };
