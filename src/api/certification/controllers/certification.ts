8767f134cd2bae388623c3bce082c23fe222c77c
Kris Stern <krisstern@outlook.com>
2022-09-12 01:26:03 +0800

    Modify Member content-type

--- src/api/certification/controllers/certification.ts
+++ src/api/certification/controllers/certification.ts
@@ -4,4 +4,47 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::certification.certification');
+export default factories.createCoreController('api::certification.certification', ({ strapi }) => ({
+  async find(ctx) {
+  // ctx.query = { ...ctx.query, local: 'en' }
+
+  const { data, meta } = await super.find(ctx);
+
+  return { data, meta };
+},
+
+async findOne(ctx) {
+  const { id } = ctx.params;
+  const { query } = ctx;
+
+  const entity = await strapi.service('api::certification.certification').findOne(id, query);
+  const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+  return this.transformResponse(sanitizedEntity);
+},
+
+async create(ctx) {
+  ctx.request.body.data = {
+    ...ctx.request.body.data,
+    createdAt: ctx.createdAt,
+    updatedAt: ctx.updatedAt
+  };
+
+  const { query } = ctx;
+
+  const allCertifications = strapi.service('api::certification.certification').find(ctx);
+
+
+
+  const result = await super.create(ctx);
+
+  return result
+},
+
+async delete(ctx) {
+
+  const result = await super.delete(ctx);
+
+  return result;
+}
+}));
